1c0bd32b49ab91510a810ab8898ba7d7c83fa943
vatsal shah <vatsal.nyusoft@gmail.com>
2022-07-09 19:03:16 +0530

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+import * as Ladda from 'ladda';
+require('./bootstrap');
+window.$ = window.jQuery = require('jquery/dist/jquery');
+require('bootstrap/dist/js/bootstrap');
+require('select2/dist/js/select2.full.min');
+require('jquery-validation');
+require('jquery-validation/dist/additional-methods');
+require('jquery-expander/jquery.expander.min');
+window.Noty = require('noty');
+window.Ladda = require('ladda');
+window.Swal = require('sweetalert2');
+require('flatpickr');
+require('./custom');
+require('./shop.min');
